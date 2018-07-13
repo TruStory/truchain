2e064d31d2786f0c82f8f8195ccafc8e6d36cf90
Shane Vitarana <shanev@gmail.com>
2018-07-13 11:56:56 -0400

    NewStory function

--- x/trustory/types.go
+++ x/trustory/types.go
@@ -1,5 +1,22 @@
 package trustory
 
 type Story struct {
-	Body string `json:"body"`
+	Body        string      `json:"body"`
+	Creator     sdk.Address `json:"creator"`
+	SubmitBlock int64       `json:"submit_block`
+	State       string      `json:"state"`
+	YesVotes    int64       `json:"yes_votes`
+	NoVotes     int64       `json:"no_votes"`
+}
+
+// NewStory creates a new story
+func NewStory(body string, creator sdk.Address, blockHeight int64) Story {
+	return Story{
+		Body:        body,
+		Creator:     creator,
+		SubmitBlock: blockHeight,
+		State:       "Created",
+		YesVotes:    0,
+		NoVotes:     0,
+	}
 }
