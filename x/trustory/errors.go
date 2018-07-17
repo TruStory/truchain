9e556e45385f4e86df3eebfc3214c081018390c6
Shane Vitarana <shanev@gmail.com>
2018-07-17 12:19:08 -0400

    implement Msg interface for story and add error handling

---
+++ x/trustory/errors.go
@@ -0,0 +1,51 @@
+package trustory
+
+import sdk "github.com/cosmos/cosmos-sdk/types"
+
+type CodeType = sdk.CodeType
+
+const (
+	DefaultCodespace sdk.CodespaceType = 7
+
+	// TruStory errors reserve 700 ~ 799.
+	CodeInvalidOption CodeType = 701
+	CodeInvalidBody   CodeType = 702
+)
+
+func codeToDefaultMsg(code CodeType) string {
+	switch code {
+	case CodeInvalidOption:
+		return "Invalid option"
+	case CodeInvalidBody:
+		return "Invalid story body"
+	default:
+		return sdk.CodeToDefaultMsg(code)
+	}
+}
+
+//----------------------------------------
+// Error constructors
+
+// ErrInvalidOption throws an error on invalid option
+func ErrInvalidOption(msg string) sdk.Error {
+	return newError(DefaultCodespace, CodeInvalidOption, msg)
+}
+
+// ErrInvalidBody throws an error on invalid title
+func ErrInvalidBody(msg string) sdk.Error {
+	return newError(DefaultCodespace, CodeInvalidTitle, msg)
+}
+
+//----------------------------------------
+
+func msgOrDefaultMsg(msg string, code CodeType) string {
+	if msg != "" {
+		return msg
+	}
+	return codeToDefaultMsg(code)
+}
+
+func newError(codespace sdk.CodespaceType, code CodeType, msg string) sdk.Error {
+	msg = msgOrDefaultMsg(msg, code)
+	return sdk.NewError(codespace, code, msg)
+}
--- x/trustory/types.go
+++ x/trustory/types.go
@@ -1,5 +1,13 @@
 package trustory
 
+import (
+	"encoding/json"
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// Story defines the basic properties of a votable story
 type Story struct {
 	Body        string      `json:"body"`
 	Creator     sdk.Address `json:"creator"`
@@ -38,10 +46,58 @@ func (s *Story) updateVote(option string, amount int64) sdk.Error {
 //--------------------------------------------------------
 //--------------------------------------------------------
 
-// SubmitStory defines a message to create a story
+// SubmitStoryMsg defines a message to create a story
 type SubmitStoryMsg struct {
 	Body    string
 	Creator sdk.Address
+}
+
+// NewSubmitStoryMsg submits a message with a new story
+func NewSubmitStoryMsg(body string, creator sdk.Address) SubmitStoryMsg {
+	return SubmitStoryMsg{
+		Body:    body,
+		Creator: creator,
+	}
+}
+
+// Type implements Msg
+func (msg SubmitStoryMsg) Type() string {
+	return "truStory"
+}
+
+// Get implement Msg
+func (msg SubmitStoryMsg) Get(key interface{}) (value interface{}) {
+	return nil
+}
+
+// GetSignBytes implements Msg
+func (msg SubmitStoryMsg) GetSignBytes() []byte {
+	b, err := json.Marshal(msg)
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+// GetSigners implements Msg
+func (msg SubmitStoryMsg) GetSigners() []sdk.Address {
+	return []sdk.Address{msg.Submitter}
+}
+
+// ValidateBasic implements Msg
+func (msg SubmitStoryMsg) ValidateBasic() sdk.Error {
+	if len(msg.Creator) == 0 {
+		return sdk.ErrInvalidAddress("Invalid address: " + msg.Creator.String())
+	}
+	if len(strings.TimeSpace(msg.Body)) <= 0 {
+		return ErrInvalidBody("Cannot submit a story with an empty body")
+	}
+
+	return nil
+}
+
+func (msg SubmitStoryMsg) String() string {
+	return fmt.Sprintf("SubmitStoryMsg{%v}", msg.Body)
 }
 
 // VoteMsg defines a message to vote on a story
