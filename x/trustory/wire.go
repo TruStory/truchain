c94e99462602f36420c8a25789d2831f51fe520f
Shane Vitarana <shanev@gmail.com>
2018-07-19 16:03:48 -0400

    added wire

--- x/trustory/wire.go
+++ x/trustory/wire.go
@@ -1,1 +1,9 @@
 package trustory
+
+import "github.com/cosmos/cosmos-sdk/wire"
+
+// RegisterWire registers messages into the wire codec
+func RegisterWire(cdc *wire.Codec) {
+	cdc.RegisterConcrete(SubmitStoryMsg{}, "trustory/SubmitStoryMsg", nil)
+	cdc.RegisterConcrete(VoteMsg{}, "trustory/VoteMsg", nil)
+}
