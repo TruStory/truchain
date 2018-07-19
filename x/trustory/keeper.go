fe3ef5a10444d0bda8bf4f580b6083c783e2d97e
Shane Vitarana <shanev@gmail.com>
2018-07-19 11:56:52 -0400

    rough keeper tests

--- x/trustory/keeper.go
+++ x/trustory/keeper.go
@@ -1,1 +1,31 @@
 package trustory
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/wire"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+	"github.com/cosmos/cosmos-sdk/x/stake"
+)
+
+// Keeper data type
+type Keeper struct {
+	TruStory  sdk.StoreKey
+	codespace sdk.CodespaceType
+	cdc       *wire.Codec
+
+	ck bank.Keeper
+	sm stake.Keeper
+}
+
+// NewKeeper creates a new keeper with write and read access
+func NewKeeper(TruStory sdk.StoreKey, ck bank.Keeper, sm stake.Keeper, codespace sdk.CodespaceType) Keeper {
+	cdc := wire.NewCodec()
+
+	return Keeper{
+		TruStory:  TruStory,
+		cdc:       cdc,
+		ck:        ck,
+		sm:        sm,
+		codespace: codespace,
+	}
+}
