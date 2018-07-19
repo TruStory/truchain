dae0f8047f0061796775ba19bf86153cceeac0b0
Shane Vitarana <shanev@gmail.com>
2018-07-19 15:31:03 -0400

    handler skeleton

--- x/trustory/handler.go
+++ x/trustory/handler.go
@@ -1,1 +1,41 @@
 package trustory
+
+import (
+	"reflect"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// NewHandler creates a new handler for all TruStory type messages
+func NewHandler(k Keeper) sdk.Handler {
+	return func(ctx sdk.Context, msg sdk.Msg) sdk.Result {
+		switch msg := msg.(type) {
+		case SubmitStoryMsg:
+			return handleSubmitStoryMsg(ctx, k, msg)
+		case VoteMsg:
+			return handleVoteMsg(ctx, k, msg)
+		default:
+			errMsg := "Unrecognized Msg type: " + reflect.TypeOf(msg).Name()
+			return sdk.ErrUnknownRequest(errMsg).Result()
+		}
+	}
+}
+
+// handleSubmitStoryMsg handles the logic of a SubmitStoryMsg
+func handleSubmitStoryMsg(ctx sdk.Context, k Keeper, msg SubmitStoryMsg) sdk.Result {
+	err := msg.ValidateBasic()
+	if err != nil {
+		return err.Result()
+	}
+
+	return sdk.Result{}
+}
+
+func handleVoteMsg(ctx sdk.Context, k Keeper, msg VoteMsg) sdk.Result {
+	err := msg.ValidateBasic()
+	if err != nil {
+		return err.Result()
+	}
+
+	return sdk.Result{}
+}
