6ef1904cc98145088f5e802c8da06979a83910dc
Shane Vitarana <shanev@gmail.com>
2018-07-19 11:56:52 -0400

    rough keeper tests

---
+++ x/trustory/keeper_test.go
@@ -0,0 +1,12 @@
+package trustory
+
+// import (
+// 	"testing"
+
+// 	"github.com/stretchr/testify/assert"
+// )
+
+// func TestTruStoryKeeper(t *testing.T) {
+// 	ctx, _, k = createTestInput(t, int64(200))
+// 	assert.NotNil(t, k)
+// }
