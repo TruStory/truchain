9224dfe61d9af66755785e185af876e72bdbbaf2
Shane Vitarana <shanev@gmail.com>
2018-08-13 10:29:17 -0700

    implmement Msg interfae for SubmitEvidenceMsg

--- x/trustory/msg.go
+++ x/trustory/msg.go
@@ -126,14 +126,50 @@ func (msg AddCommentMsg) GetSigners() []types.Address {
 type SubmitEvidenceMsg struct {
 	StoryID int64          `json:"story_id"`
 	Creator sdk.AccAddress `json:"creator"`
-	URL     url.URL        `json:"url"`
+	URI     string         `json:"url"`
 }
 
 // NewSubmitEvidenceMsg creates a new message to submit evidence for a story
-func NewSubmitEvidenceMsg(storyID int64, creator sdk.AccAddress, url url.URL) SubmitEvidenceMsg {
+func NewSubmitEvidenceMsg(storyID int64, creator sdk.AccAddress, uri string) SubmitEvidenceMsg {
 	return SubmitEvidenceMsg{
 		StoryID: storyID,
 		Creator: creator,
-		URL:     url,
+		URI:     uri,
 	}
 }
+
+// Type implements Msg
+func (msg SubmitEvidenceMsg) Type() string {
+	return "truStory"
+}
+
+// GetSignBytes implements Msg
+func (msg SubmitEvidenceMsg) GetSignBytes() []byte {
+	b, err := json.Marshal(msg)
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+// ValidateBasic implements Msg
+func (msg SubmitEvidenceMsg) ValidateBasic() types.Error {
+	if msg.StoryID <= 0 {
+		return ErrInvalidStoryID("StoryID cannot be negative")
+	}
+	if len(msg.Creator) == 0 {
+		return sdk.ErrInvalidAddress("Invalid address: " + msg.Creator.String())
+	}
+	err := url.ParseRequestURI(msg.URI)
+	if err != nil {
+		return sdk.ErrInvalidURL("Invalid URL: " + msg.URI.String())
+	}
+	return nil
+}
+
+// GetSigners implements Msg
+func (msg SubmitEvidenceMsg) GetSigners() []types.Address {
+	return []sdk.AccAddress{msg.Creator}
+}
+
+// ============================================================================
