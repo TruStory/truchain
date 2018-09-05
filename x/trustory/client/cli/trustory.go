2dd463ff50d5be4d2729ac1c6279f1959a7f34ec
Shane Vitarana <shanev@gmail.com>
2018-09-04 23:04:00 -0700

    Added escrow account to story type

--- x/trustory/client/cli/trustory.go
+++ x/trustory/client/cli/trustory.go
@@ -1,10 +1,6 @@
 package cli
 
 import (
-	"fmt"
-
-	ts "github.com/TruStory/trucoin/x/trustory/types"
-	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/wire"
 	"github.com/spf13/cobra"
 )
@@ -18,40 +14,41 @@ const (
 
 // GetCmdQueryStories gets the command to get all stories
 func GetCmdQueryStories(storeName string, cdc *wire.Codec) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "stories",
-		Short: "Query all stories",
-		Args:  cobra.ExactArgs(0),
-		Run: func(cmd *cobra.Command, args []string) {
-			ctx := context.NewCoreContextFromViper()
+	// cmd := &cobra.Command{
+	// 	Use:   "stories",
+	// 	Short: "Query all stories",
+	// 	Args:  cobra.ExactArgs(0),
+	// 	Run: func(cmd *cobra.Command, args []string) {
+	// 		ctx := context.NewCoreContextFromViper()
 
-			resKVs, err := ctx.QuerySubspace(cdc, []byte("stories"), storeName)
-			if err != nil {
-				// return err
-			}
+	// 		resKVs, err := ctx.QuerySubspace(cdc, []byte("stories"), storeName)
+	// 		if err != nil {
+	// 			// return err
+	// 		}
 
-			// if viper.IsSet(FlagVerifiedStory) {
-			// 	isVerified := viper.GetBool(FlagVerifiedStory)
-			// } else {
-			// 	isVerified := false
-			// }
+	// 		// if viper.IsSet(FlagVerifiedStory) {
+	// 		// 	isVerified := viper.GetBool(FlagVerifiedStory)
+	// 		// } else {
+	// 		// 	isVerified := false
+	// 		// }
 
-			// parse out the stories
-			var stories []ts.Story
-			for _, KV := range resKVs {
-				var story ts.Story
-				cdc.MustUnmarshalBinary(KV.Value, &story)
-				stories = append(stories, story)
-			}
+	// 		// parse out the stories
+	// 		var stories []ts.Story
+	// 		for _, KV := range resKVs {
+	// 			var story ts.Story
+	// 			cdc.MustUnmarshalBinary(KV.Value, &story)
+	// 			stories = append(stories, story)
+	// 		}
 
-			output, err := wire.MarshalJSONIndent(cdc, stories)
-			if err != nil {
-				// return err
-			}
-			fmt.Println(string(output))
-			// return nil
-		},
-	}
-	cmd.Flags().Bool(FlagVerifiedStory, false, "Query only verified stories (default: true)")
-	return cmd
+	// 		output, err := wire.MarshalJSONIndent(cdc, stories)
+	// 		if err != nil {
+	// 			// return err
+	// 		}
+	// 		fmt.Println(string(output))
+	// 		// return nil
+	// 	},
+	// }
+	// cmd.Flags().Bool(FlagVerifiedStory, false, "Query only verified stories (default: true)")
+	// return cmd
+	return nil
 }
