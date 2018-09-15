02c7b752396532ce1c20a30fbc29f611d6d46bae
Shane Vitarana <shanev@gmail.com>
2018-09-14 21:51:17 -0700

    Added new stories to active (in-progress) story queue

--- x/trustory/db/blocker_test.go
+++ x/trustory/db/blocker_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"testing"
+	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/bank"
@@ -10,14 +11,41 @@ import (
 	"github.com/tendermint/tendermint/libs/log"
 )
 
-func TestNewResponseEndBlock(t *testing.T) {
+func TestNotMeetVoteMinNewResponseEndBlock(t *testing.T) {
 	ms, storyKey, voteKey := setupMultiStore()
 	cdc := makeCodec()
 	k := NewTruKeeper(storyKey, voteKey, bank.Keeper{}, cdc)
-	ctx := sdk.NewContext(ms, abci.Header{}, false, log.NewNopLogger())
+
+	// create fake context with fake block time in header
+	time := time.Date(2018, time.September, 14, 23, 0, 0, 0, time.UTC)
+	header := abci.Header{Time: time}
+	ctx := sdk.NewContext(ms, header, false, log.NewNopLogger())
+
+	// create fake story with vote end after block time
 	_ = createFakeStory(ms, k)
 
 	r := k.NewResponseEndBlock(ctx)
 	assert.NotNil(t, r)
-	assert.Equal(t, abci.ResponseEndBlock{}, r, "Invalid response end block")
+}
+
+func TestMeetVoteMinNewResponseEndBlock(t *testing.T) {
+	ms, storyKey, voteKey := setupMultiStore()
+	cdc := makeCodec()
+	k := NewTruKeeper(storyKey, voteKey, bank.Keeper{}, cdc)
+
+	// create fake context with fake block time in header
+	time := time.Date(2018, time.September, 14, 23, 0, 0, 0, time.UTC)
+	header := abci.Header{Time: time}
+	ctx := sdk.NewContext(ms, header, false, log.NewNopLogger())
+
+	// create fake story with vote end after block time
+	storyID := createFakeStory(ms, k)
+
+	fakeAddr := sdk.AccAddress([]byte{1, 2})
+	fakeCoins, _ := sdk.ParseCoins("10memecoin")
+
+	_, _ = k.VoteStory(ctx, storyID, fakeAddr, true, fakeCoins)
+
+	r := k.NewResponseEndBlock(ctx)
+	assert.NotNil(t, r)
 }
