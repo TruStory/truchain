116133abe404d2ec03e5f34a9f7362829afacd7c
Shane Vitarana <shanev@gmail.com>
2018-09-18 11:47:20 -0700

    Broke up story and vote keeper into separate files

--- x/trustory/db/keeper_test.go
+++ x/trustory/db/keeper_test.go
@@ -1,16 +1,13 @@
 package db
 
 import (
-	"testing"
 	"time"
 
 	ts "github.com/TruStory/trucoin/x/trustory/types"
 	"github.com/cosmos/cosmos-sdk/store"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth"
-	"github.com/cosmos/cosmos-sdk/x/bank"
 
-	"github.com/stretchr/testify/assert"
 	amino "github.com/tendermint/go-amino"
 	abci "github.com/tendermint/tendermint/abci/types"
 
@@ -20,97 +17,14 @@ import (
 	"github.com/tendermint/tendermint/crypto/encoding/amino"
 )
 
-func TestAddGetStory(t *testing.T) {
-	ms, _, storyKey, voteKey := setupMultiStore()
-	cdc := makeCodec()
-	keeper := NewTruKeeper(storyKey, voteKey, bank.Keeper{}, cdc)
-	ctx := sdk.NewContext(ms, abci.Header{}, false, log.NewNopLogger())
-	storyID := createFakeStory(ms, keeper)
-
-	// test getting a non-existant story
-	_, err := keeper.GetStory(ctx, int64(5))
-	assert.NotNil(t, err)
-
-	// test getting an existing story
-	savedStory, err := keeper.GetStory(ctx, storyID)
-	assert.Nil(t, err)
-
-	ti := time.Date(2018, time.September, 13, 23, 0, 0, 0, time.UTC)
-
-	story := ts.Story{
-		ID:           storyID,
-		Body:         "Body of story.",
-		Category:     ts.DEX,
-		CreatedBlock: int64(0),
-		Creator:      sdk.AccAddress([]byte{1, 2}),
-		Escrow:       sdk.AccAddress([]byte{3, 4}),
-		State:        ts.Created,
-		StoryType:    ts.Default,
-		VoteStart:    ti,
-		VoteEnd:      ti,
-	}
-
-	assert.Equal(t, savedStory, story, "Story received from store does not match expected value")
-
-	// test incrementing id by adding another story
-	body := "Body of story 2."
-	category := ts.Bitcoin
-	creator := sdk.AccAddress([]byte{3, 4})
-	escrow := sdk.AccAddress([]byte{4, 5})
-	storyType := ts.Default
-
-	storyID, _ = keeper.AddStory(ctx, body, category, creator, escrow, storyType, time.Now(), time.Now())
-	assert.Equal(t, int64(1), storyID, "Story ID did not increment properly")
-}
-
-func TestVoteStory(t *testing.T) {
-	ms, _, storyKey, voteKey := setupMultiStore()
-	cdc := makeCodec()
-	keeper := NewTruKeeper(storyKey, voteKey, bank.Keeper{}, cdc)
-	ctx := sdk.NewContext(ms, abci.Header{}, false, log.NewNopLogger())
-
-	storyID := createFakeStory(ms, keeper)
-
-	creator := sdk.AccAddress([]byte{3, 4})
-	vote := true
-	stake, _ := sdk.ParseCoins("10trustake")
-
-	// test voting on a non-existant story
-	_, err := keeper.VoteStory(ctx, int64(5), creator, vote, stake)
-	assert.NotNil(t, err)
-
-	// test voting on a story
-	voteID, err := keeper.VoteStory(ctx, storyID, creator, vote, stake)
-	assert.Nil(t, err)
-	assert.Equal(t, voteID, int64(0), "Vote ID does not match")
-
-	// test getting a non-existant vote
-	_, err = keeper.GetVote(ctx, int64(5))
-	assert.NotNil(t, err)
-
-	// test getting vote and comparing fields
-	savedVote, err := keeper.GetVote(ctx, voteID)
-	assert.Nil(t, err)
-	assert.Equal(t, savedVote.Vote, true, "Vote choice  does not match")
-
-	assert.Equal(t, savedVote.Amount.AmountOf("trustake"), sdk.NewInt(10), "Vote amount does not match")
-}
-
-// ============================================================================
+// Helper functions used for keeper tests
 
 func createFakeStory(ms sdk.MultiStore, k TruKeeper) int64 {
 	ctx := sdk.NewContext(ms, abci.Header{}, false, log.NewNopLogger())
 	body := "Body of story."
 	category := ts.DEX
 	creator := sdk.AccAddress([]byte{1, 2})
-
-	// add coins to escrow account
 	escrow := sdk.AccAddress([]byte{3, 4})
-	// coins, _ := sdk.ParseCoins("100memecoin")
-	// _ = k.ck.SetCoins(ctx, escrow, coins)
-	// hasCoins := k.ck.HasCoins(ctx, escrow, coins)
-	// fmt.Print(hasCoins)
-
 	storyType := ts.Default
 	t := time.Date(2018, time.September, 13, 23, 0, 0, 0, time.UTC)
 
