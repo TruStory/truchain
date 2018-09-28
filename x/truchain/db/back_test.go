07b6df6b4e8254bd223fc386e778a2fab6d750e7
Shane Vitarana <shanev@gmail.com>
2018-09-28 16:40:01 -0700

    Test for backing interest

--- x/truchain/db/back_test.go
+++ x/truchain/db/back_test.go
@@ -1,1 +1,23 @@
 package db
+
+import (
+	"testing"
+	"time"
+
+	ts "github.com/TruStory/truchain/x/truchain/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_calculateInterest(t *testing.T) {
+	// ctx, _, _, _ := mockDB()
+
+	category := ts.DEX
+	amount := sdk.NewCoin("cat", sdk.NewInt(5))
+	period := 5 * 24 * time.Hour
+	params := ts.NewBackingParams()
+
+	interest := calculateInterest(category, amount, period, params)
+	assert.Equal(t, interest.Amount, sdk.NewInt(5), "Interest is wrong")
+}
