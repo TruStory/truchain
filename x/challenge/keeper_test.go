e039c3e8332ca4db09259ce8c242ad5d182fa914
Shane Vitarana <shanev@gmail.com>
2018-10-12 11:51:35 -0700

    Added basic challenge types

---
+++ x/challenge/keeper_test.go
@@ -0,0 +1,55 @@
+package challenge
+
+// import (
+// 	"testing"
+
+// 	sdk "github.com/cosmos/cosmos-sdk/types"
+// 	"github.com/stretchr/testify/assert"
+// )
+
+// func TestAddGetStory(t *testing.T) {
+// 	ctx, sk, ck := mockDB()
+
+// 	// test getting a non-existant story
+// 	_, err := sk.GetStory(ctx, int64(5))
+// 	assert.NotNil(t, err)
+
+// 	storyID := createFakeStory(ctx, sk, ck)
+
+// 	// test getting an existing story
+// 	savedStory, err := sk.GetStory(ctx, storyID)
+// 	assert.Nil(t, err)
+
+// 	story := Story{
+// 		ID:           storyID,
+// 		Body:         "Body of story.",
+// 		CategoryID:   int64(1),
+// 		CreatedBlock: int64(0),
+// 		Creator:      sdk.AccAddress([]byte{1, 2}),
+// 		State:        Created,
+// 		Kind:         Default,
+// 	}
+
+// 	assert.Equal(t, story, savedStory, "Story received from store does not match expected value")
+
+// 	// test incrementing id by adding another story
+// 	body := "Body of story 2."
+// 	creator := sdk.AccAddress([]byte{3, 4})
+// 	kind := Default
+
+// 	storyID, _ = sk.NewStory(ctx, body, int64(1), creator, kind)
+// 	assert.Equal(t, int64(2), storyID, "Story ID did not increment properly")
+// }
+
+// func TestGetStoriesWithCategory(t *testing.T) {
+// 	ctx, sk, ck := mockDB()
+
+// 	numStories := 100
+
+// 	for i := 0; i < numStories; i++ {
+// 		createFakeStory(ctx, sk, ck)
+// 	}
+
+// 	stories, _ := sk.GetStoriesWithCategory(ctx, 1)
+// 	assert.Equal(t, numStories, len(stories))
+// }
---
+++ x/challenge/test_common.go
@@ -0,0 +1,56 @@
+package challenge
+
+// import (
+// 	c "github.com/TruStory/truchain/x/category"
+// 	"github.com/cosmos/cosmos-sdk/store"
+// 	sdk "github.com/cosmos/cosmos-sdk/types"
+// 	amino "github.com/tendermint/go-amino"
+// 	abci "github.com/tendermint/tendermint/abci/types"
+// 	"github.com/tendermint/tendermint/crypto/encoding/amino"
+// 	dbm "github.com/tendermint/tendermint/libs/db"
+// 	"github.com/tendermint/tendermint/libs/log"
+// )
+
+// func mockDB() (sdk.Context, Keeper, c.Keeper) {
+// 	db := dbm.NewMemDB()
+
+// 	storyKey := sdk.NewKVStoreKey("stories")
+// 	catKey := sdk.NewKVStoreKey("categories")
+
+// 	ms := store.NewCommitMultiStore(db)
+// 	ms.MountStoreWithDB(storyKey, sdk.StoreTypeIAVL, db)
+// 	ms.MountStoreWithDB(catKey, sdk.StoreTypeIAVL, db)
+// 	ms.LoadLatestVersion()
+
+// 	ctx := sdk.NewContext(ms, abci.Header{}, false, log.NewNopLogger())
+
+// 	codec := amino.NewCodec()
+// 	cryptoAmino.RegisterAmino(codec)
+// 	RegisterAmino(codec)
+
+// 	ck := c.NewKeeper(catKey, storyKey, codec)
+// 	sk := NewKeeper(storyKey, catKey, ck, codec)
+
+// 	return ctx, sk, ck
+// }
+
+// func createFakeStory(ctx sdk.Context, sk Keeper, ck c.ReadWriteKeeper) int64 {
+// 	body := "Body of story."
+// 	cat := createFakeCategory(ctx, ck)
+// 	creator := sdk.AccAddress([]byte{1, 2})
+// 	storyType := Default
+
+// 	storyID, _ := sk.NewStory(ctx, body, cat.ID, creator, storyType)
+
+// 	return storyID
+// }
+
+// func createFakeCategory(ctx sdk.Context, ck c.ReadWriteKeeper) c.Category {
+// 	existing, err := ck.GetCategory(ctx, 1)
+// 	if err == nil {
+// 		return existing
+// 	}
+// 	id, _ := ck.NewCategory(ctx, "decentralized exchanges", "trudex", "category for experts in decentralized exchanges")
+// 	cat, _ := ck.GetCategory(ctx, id)
+// 	return cat
+// }
