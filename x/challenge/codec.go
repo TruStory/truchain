d9fec1d4831f4628ce47b8c43e6f94ff64e2c149
Shane Vitarana <shanev@gmail.com>
2018-10-12 22:57:05 -0700

    Added challenge codec

--- x/challenge/codec.go
+++ x/challenge/codec.go
@@ -1,11 +1,10 @@
 package challenge
 
-// import (
-// 	amino "github.com/tendermint/go-amino"
-// )
+import (
+	amino "github.com/tendermint/go-amino"
+)
 
-// // RegisterAmino registers messages into the codec
-// func RegisterAmino(c *amino.Codec) {
-// 	c.RegisterConcrete(SubmitEvidenceMsg{}, "story/SubmitEvidenceMsg", nil)
-// 	c.RegisterConcrete(SubmitStoryMsg{}, "story/SubmitStoryMsg", nil)
-// }
+// RegisterAmino registers messages into the codec
+func RegisterAmino(c *amino.Codec) {
+	c.RegisterConcrete(StartChallengeMsg{}, "challenge/StartChallengeMsg", nil)
+}
