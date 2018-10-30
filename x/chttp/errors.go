36ca3a5d493c28d21eba03836fa0fa8b307b9d62
Duncan Smith <hi@dms.sh>
2018-10-29 23:26:15 -0700

    Add API and support
    - Start HTTP server during first beginBlocker
    - Serve HTTP using `chttp`
    - Resolve GraphQL queries over HTTP
    - Accept presigned messages over HTTP
    - Register keys over HTTP
    - Integration-test API using Just-API
    - Destructable test environment so tests can be re-run at will

---
+++ x/chttp/errors.go
@@ -0,0 +1,34 @@
+package chttp
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	tcmn "github.com/tendermint/tendermint/libs/common"
+)
+
+func signBytesMismatchError(received tcmn.HexBytes, canonical tcmn.HexBytes) error {
+	s := "Tx Error: Bytes signed were \"%s\" but canonical tx encoding would be \"%s\" (utf8: \"%s\")"
+	cs, _ := hex.DecodeString(canonical.String())
+	return fmt.Errorf(s, received.String(), canonical.String(), cs)
+}
+
+func expectedMoreMessagesError(receivedCount int, providedTypes []string) error {
+	s := "Tx Error: Only got first %i messages in expected sequence %v"
+	return fmt.Errorf(s, receivedCount, providedTypes)
+}
+
+func internalDecodingError(detail string) error {
+	s := "Internal Decoding Error: %s"
+	return fmt.Errorf(s, detail)
+}
+
+func unsupportedMsgTypeError(name string, supported []string) error {
+	s := "Tx Error: Unsupported message type \"%s\" (supported: %v)"
+	return fmt.Errorf(s, name, supported)
+}
+
+func unsupportedAlgoError(name string, supported []string) error {
+	s := "Tx Error: Unsupported public key algorithm \"%s\" (supported: %v)"
+	return fmt.Errorf(s, name, supported)
+}
