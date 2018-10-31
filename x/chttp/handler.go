f471ce3357690d1e4d113fcb988d6ade973d2cee
Duncan Smith <hi@dms.sh>
2018-10-31 02:45:54 -0700

    Please Gometalinter

--- x/chttp/handler.go
+++ x/chttp/handler.go
@@ -1,8 +1,8 @@
 package chttp
 
 import (
-	"fmt"
-	"net/http"
+  "fmt"
+  "net/http"
 )
 
 // Handler is an http.Handler that renders a chttp.Response
@@ -10,16 +10,20 @@ type Handler func(*http.Request) Response
 
 // HandlerFunc wraps a `chttp.Handler` in a standard `http` handler
 func (h Handler) HandlerFunc() func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		res := h(r)
-		bs, err := res.Marshal()
+  return func(w http.ResponseWriter, r *http.Request) {
+    res := h(r)
+    bs, err := res.Marshal()
 
-		if err != nil {
-			fmt.Println("INTERNAL DECODING ERROR: ", err, string(res.Data()))
-			panic(err)
-		}
+    if err != nil {
+      fmt.Println("INTERNAL DECODING ERROR: ", err, string(res.Data()))
+      panic(err)
+    }
 
-		w.WriteHeader(res.HTTPCode())
-		w.Write(bs)
-	}
+    w.WriteHeader(res.HTTPCode())
+    _, err = w.Write(bs)
+
+    if err != nil {
+      panic(err)
+    }
+  }
 }
