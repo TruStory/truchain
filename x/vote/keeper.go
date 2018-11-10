7320f2f6d93ea98bb672991b4dfc4acce709512d
Shane Vitarana <shanev@gmail.com>
2018-11-09 22:54:03 -0800

    Added vote module and initial files

---
+++ x/vote/keeper.go
@@ -0,0 +1,179 @@
+package vote
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/TruStory/truchain/x/game"
+	"github.com/TruStory/truchain/x/story"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+
+	app "github.com/TruStory/truchain/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	amino "github.com/tendermint/go-amino"
+)
+
+// ReadKeeper defines a module interface that facilitates read only access to truchain data
+type ReadKeeper interface {
+	app.ReadKeeper
+
+	Get(ctx sdk.Context, id int64) (vote app.Vote, err sdk.Error)
+	GetByGame(ctx sdk.Context, gameID int64) ([]app.Vote, sdk.Error)
+}
+
+// WriteKeeper defines a module interface that facilities write only access to truchain data
+type WriteKeeper interface {
+	Create(
+		ctx sdk.Context, storyID int64, amount sdk.Coin,
+		choice bool, comment string, creator sdk.AccAddress,
+		evidence []url.URL) (int64, sdk.Error)
+
+	NewResponseEndBlock(ctx sdk.Context) sdk.Tags
+}
+
+// ReadWriteKeeper defines a module interface that facilities read/write access to truchain data
+type ReadWriteKeeper interface {
+	ReadKeeper
+	WriteKeeper
+}
+
+// Keeper data type storing keys to the key-value store
+type Keeper struct {
+	app.Keeper
+
+	storyKeeper story.WriteKeeper
+	gameKeeper  game.WriteKeeper
+	bankKeeper  bank.Keeper
+}
+
+// NewKeeper creates a new keeper with write and read access
+func NewKeeper(
+	storeKey sdk.StoreKey,
+	storyKeeper story.WriteKeeper,
+	gameKeeper game.WriteKeeper,
+	bankKeeper bank.Keeper,
+	codec *amino.Codec) Keeper {
+
+	return Keeper{
+		app.NewKeeper(codec, storeKey),
+		storyKeeper,
+		gameKeeper,
+		bankKeeper,
+	}
+}
+
+// ============================================================================
+
+// Create adds a new challenge on a story in the KVStore
+func (k Keeper) Create(
+	ctx sdk.Context, storyID int64, amount sdk.Coin,
+	choice bool, comment string, creator sdk.AccAddress,
+	evidence []url.URL) (int64, sdk.Error) {
+
+	// get the story
+	story, err := k.storyKeeper.GetStory(ctx, storyID)
+	if err != nil {
+		return 0, err
+	}
+
+	// TODO: check if this voter has already cast a vote
+
+	// create a new vote
+	vote := app.Vote{
+		ID:        k.GetNextID(ctx),
+		Amount:    amount,
+		Comment:   comment,
+		Creator:   creator,
+		Evidence:  evidence,
+		Vote:      choice,
+		Timestamp: app.NewTimestamp(ctx.BlockHeader()),
+	}
+
+	// persist vote
+	k.set(ctx, vote)
+
+	// persist game <-> vote association
+	k.setGameVotePair(ctx, story.GameID, vote.ID)
+
+	return vote.ID, nil
+}
+
+// Get returns a `Vote` from the KVStore
+func (k Keeper) Get(ctx sdk.Context, id int64) (vote app.Vote, err sdk.Error) {
+	store := k.GetStore(ctx)
+	bz := store.Get(k.GetIDKey(id))
+	if bz == nil {
+		// TODO: add error
+		return vote, sdk.ErrInternal("ErrNotFound(id)")
+	}
+	k.GetCodec().MustUnmarshalBinary(bz, &vote)
+
+	return
+}
+
+// GetByChallenge returns a list of votes for a given challenge
+func (k Keeper) GetByChallenge(
+	ctx sdk.Context, challengeID int64) (votes []app.Vote, err sdk.Error) {
+
+	store := k.GetStore(ctx)
+
+	// builds prefix for game <-> vote association
+	prefix := k.gameVotePrefix(challengeID)
+
+	// iterates through keyspace to find all votes on a challenge
+	iter := sdk.KVStorePrefixIterator(store, prefix)
+	defer iter.Close()
+	for ; iter.Valid(); iter.Next() {
+		var vote app.Vote
+		bz := iter.Value()
+		if bz == nil {
+			// TODO:
+			return votes, sdk.ErrInternal("ErrNotFoundByChallenge(id)")
+		}
+		k.GetCodec().MustUnmarshalBinary(bz, &vote)
+		votes = append(votes, vote)
+	}
+
+	return
+}
+
+// ============================================================================
+
+// "games:id:5:votes:id:10" -> int64
+func (k Keeper) gameVoteKey(gameID int64, voteID int64) []byte {
+	prefix := fmt.Sprintf(
+		"%s:id:%d:%s:id:%d",
+		k.gameKeeper.GetStoreKey().Name(), gameID,
+		k.GetStoreKey().Name(), voteID)
+
+	return []byte(prefix)
+}
+
+// "games:id:5:votes:id:"
+func (k Keeper) gameVotePrefix(challengeID int64) []byte {
+	prefix := fmt.Sprintf(
+		"%s:id:%d:%s:id:",
+		k.gameKeeper.GetStoreKey().Name(),
+		challengeID,
+		k.GetStoreKey().Name())
+
+	return []byte(prefix)
+}
+
+// ============================================================================
+
+// saves a `Vote` in the KVStore
+func (k Keeper) set(ctx sdk.Context, vote app.Vote) {
+	store := k.GetStore(ctx)
+	store.Set(
+		k.GetIDKey(vote.ID),
+		k.GetCodec().MustMarshalBinary(vote))
+}
+
+// saves a game <-> vote association in the store
+func (k Keeper) setGameVotePair(ctx sdk.Context, gameID int64, voteID int64) {
+	store := k.GetStore(ctx)
+	store.Set(
+		k.gameVoteKey(gameID, voteID),
+		k.GetCodec().MustMarshalBinary(""))
+}
