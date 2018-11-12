c47d20c4362474aebde40c9ab37aa4d967a553f3
Shane Vitarana <shanev@gmail.com>
2018-11-11 22:05:10 -0800

    Refactored challenge <-> game mappings using UserList type

--- x/challenge/keeper_test.go
+++ x/challenge/keeper_test.go
@@ -32,10 +32,33 @@ func TestNewGetChallenge(t *testing.T) {
 	id, err := k.Create(ctx, storyID, amount, argument, creator, evidence)
 	assert.Nil(t, err)
 
-	challenge, err := k.Get(ctx, id)
+	challenge, err := k.Challenge(ctx, id)
 	assert.Nil(t, err)
 
 	assert.Equal(t, amount, challenge.Amount, "should match")
+}
+
+func TestChallengesByGame(t *testing.T) {
+	ctx, k, sk, ck, bankKeeper := mockDB()
+
+	storyID := createFakeStory(ctx, sk, ck)
+	amount := sdk.NewCoin("trudex", sdk.NewInt(15))
+	argument := "test argument is long enough"
+	cnn, _ := url.Parse("http://www.cnn.com")
+	evidence := []url.URL{*cnn}
+
+	creator := sdk.AccAddress([]byte{1, 2})
+	bankKeeper.AddCoins(ctx, creator, sdk.Coins{amount})
+
+	creator2 := sdk.AccAddress([]byte{3, 4})
+	bankKeeper.AddCoins(ctx, creator2, sdk.Coins{amount})
+
+	k.Create(ctx, storyID, amount, argument, creator, evidence)
+	k.Create(ctx, storyID, amount, argument, creator2, evidence)
+
+	story, _ := sk.GetStory(ctx, storyID)
+	challenges, _ := k.ChallengesByGame(ctx, story.GameID)
+	assert.Equal(t, 2, len(challenges))
 }
 
 func TestNewChallenge_Duplicate(t *testing.T) {
@@ -81,7 +104,7 @@ func TestNewChallenge_MultipleChallengers(t *testing.T) {
 	id, err := k.Create(ctx, storyID, challengeAmount, argument, creator1, evidence)
 	assert.Nil(t, err)
 
-	challenge, _ := k.Get(ctx, id)
+	challenge, _ := k.Challenge(ctx, id)
 
 	_, err = k.Create(ctx, challenge.ID, amount, argument, creator2, evidence)
 	assert.Nil(t, err)
