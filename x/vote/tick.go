677d6809dd6d61e8ba8e312d13a52e402c7d7108
Shane Vitarana <shanev@gmail.com>
2018-11-26 14:41:11 -0500

    Added returning funds for expired games
    Fixes #126
    
    Iterates through all challenges and token votes to
    return funds back when a validation game expires. Namely,
    when voting period is over and the minimum quorum is
    not met.

--- x/vote/tick.go
+++ x/vote/tick.go
@@ -1,6 +1,7 @@
 package vote
 
 import (
+	app "github.com/TruStory/truchain/types"
 	queue "github.com/cosmos/cosmos-sdk/store"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -10,7 +11,7 @@ func (k Keeper) NewResponseEndBlock(ctx sdk.Context) sdk.Tags {
 	store := ctx.KVStore(k.activeGamesQueueKey)
 	q := queue.NewQueue(k.GetCodec(), store)
 
-	err := checkGames(ctx, k, q)
+	err := k.checkGames(ctx, q)
 	if err != nil {
 		panic(err)
 	}
@@ -22,10 +23,10 @@ func (k Keeper) NewResponseEndBlock(ctx sdk.Context) sdk.Tags {
 
 // checkGames checks to see if a validation game has ended.
 // It calls itself recursively until all games have been processed.
-func checkGames(ctx sdk.Context, k Keeper, q queue.Queue) (err sdk.Error) {
+func (k Keeper) checkGames(ctx sdk.Context, gameQueue queue.Queue) sdk.Error {
 	// check the head of the queue
 	var gameID int64
-	if err := q.Peek(&gameID); err != nil {
+	if err := gameQueue.Peek(&gameID); err != nil {
 		return nil
 	}
 
@@ -35,13 +36,33 @@ func checkGames(ctx sdk.Context, k Keeper, q queue.Queue) (err sdk.Error) {
 		return err
 	}
 
+	blockTime := ctx.BlockHeader().Time
+
+	// handle expired games
+	if game.Expired(blockTime) {
+		// remove from queue
+		gameQueue.Pop()
+
+		// return funds
+		k.returnFunds(ctx, gameID)
+
+		// update story
+		err = k.storyKeeper.ExpireGame(ctx, game.StoryID)
+		if err != nil {
+			return err
+		}
+
+		// process next game
+		return k.checkGames(ctx, gameQueue)
+	}
+
 	// terminate recursion on finding the first non-ended game
-	if game.Ended(ctx.BlockHeader().Time) {
+	if game.Ended(blockTime) {
 		return nil
 	}
 
 	// remove ended game from queue
-	q.Pop()
+	gameQueue.Pop()
 
 	// process ended game
 	err = processGame(ctx, k, game)
@@ -50,5 +71,39 @@ func checkGames(ctx sdk.Context, k Keeper, q queue.Queue) (err sdk.Error) {
 	}
 
 	// check next game
-	return checkGames(ctx, k, q)
+	return k.checkGames(ctx, gameQueue)
+}
+
+func (k Keeper) returnFunds(ctx sdk.Context, gameID int64) sdk.Error {
+	// get challenges
+	challenges, err := k.challengeKeeper.ChallengesByGame(ctx, gameID)
+	if err != nil {
+		return err
+	}
+
+	// get token votes
+	tokenVotes, err := k.TokenVotesByGame(ctx, gameID)
+	if err != nil {
+		return err
+	}
+
+	// collate votes
+	var votes []app.Voter
+	for _, v := range challenges {
+		votes = append(votes, v)
+	}
+	for _, v := range tokenVotes {
+		votes = append(votes, v)
+	}
+
+	// return funds
+	for _, v := range votes {
+		_, _, err = k.bankKeeper.AddCoins(
+			ctx, v.Creator(), sdk.Coins{v.Amount()})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
