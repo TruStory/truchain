b7e2d094b116e4ab303bb87569f6a4bb4674b95d
Shane Vitarana <shanev@gmail.com>
2018-11-28 21:54:47 -0800

    Renamed add evidence message

--- x/story/codec.go
+++ x/story/codec.go
@@ -6,6 +6,6 @@ import (
 
 // RegisterAmino registers messages into the codec
 func RegisterAmino(c *amino.Codec) {
-	c.RegisterConcrete(SubmitEvidenceMsg{}, "story/SubmitEvidenceMsg", nil)
+	c.RegisterConcrete(AddEvidenceMsg{}, "story/SubmitEvidenceMsg", nil)
 	c.RegisterConcrete(SubmitStoryMsg{}, "story/SubmitStoryMsg", nil)
 }
