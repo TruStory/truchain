5e6c893bce6dde3059dfc7f4572f5b1af01c4548
Shane Vitarana <shanev@gmail.com>
2018-11-29 17:01:38 -0800

    Added multiple argument support for story

--- x/story/msg_test.go
+++ x/story/msg_test.go
@@ -75,6 +75,24 @@ func TestValidAddEvidencetMsg(t *testing.T) {
 	assert.Equal(t, []sdk.AccAddress{validCreator}, msg.GetSigners())
 }
 
+func TestValidAddArgumentMsg(t *testing.T) {
+	validStoryID := int64(1)
+	validCreator := sdk.AccAddress([]byte{1, 2})
+	validArgument := "another argument"
+	msg := NewAddArgumentMsg(validStoryID, validCreator, validArgument)
+	err := msg.ValidateBasic()
+
+	assert.Nil(t, err)
+	assert.Equal(t, "story", msg.Type())
+	assert.Equal(t, "add_argument", msg.Route())
+	assert.Equal(
+		t,
+		`{"argument":"another argument","creator":"cosmos1qypq36vzru","story_id":1}`,
+		fmt.Sprintf("%s", msg.GetSignBytes()),
+	)
+	assert.Equal(t, []sdk.AccAddress{validCreator}, msg.GetSigners())
+}
+
 func TestValidFlagStoryMsg(t *testing.T) {
 	validStoryID := int64(1)
 	validCreator := sdk.AccAddress([]byte{1, 2})
