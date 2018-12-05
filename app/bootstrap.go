a7e49a4077814321cb4040419774e020f61f3e2d
Shane Vitarana <shanev@gmail.com>
2018-12-04 23:41:55 -0800

    Generating fake story on each block

--- app/bootstrap.go
+++ app/bootstrap.go
@@ -5,6 +5,7 @@ import (
 	"net/url"
 	"time"
 
+	tru "github.com/TruStory/truchain/types"
 	"github.com/TruStory/truchain/x/story"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
@@ -15,12 +16,18 @@ func createFakeStory(ctx sdk.Context, sk story.WriteKeeper) int64 {
 
 	ctx = ctx.WithBlockHeader(abci.Header{Time: time.Now().UTC()})
 
-	// cat := createFakeCategory(ctx, ck)
 	catID := int64(1)
 	creator := sdk.AccAddress([]byte{1, 2})
 	storyType := story.Default
 	source := url.URL{}
-	evidence := []story.Evidence{}
+
+	url, _ := url.Parse("http://shanesbrain.net")
+	e := story.Evidence{
+		Creator:   creator,
+		URL:       *url,
+		Timestamp: tru.NewTimestamp(ctx.BlockHeader()),
+	}
+	evidence := []story.Evidence{e}
 	argument := []story.Argument{}
 
 	storyID, _ := sk.NewStory(ctx, argument, body, catID, creator, evidence, source, storyType)
