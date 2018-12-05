afb5f13bf893eff1c8574eef7d1c1581d7e4117a
Shane Vitarana <shanev@gmail.com>
2018-12-04 18:52:43 -0800

    Added querier for getting a game by id

---
+++ types/queryable.go
@@ -0,0 +1,30 @@
+package types
+
+import (
+	"encoding/json"
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+)
+
+// These are helpers used by Queriers
+
+// UnmarshalQueryParams unmarshals the request query from a client
+func UnmarshalQueryParams(req abci.RequestQuery, params interface{}) (sdkErr sdk.Error) {
+	parseErr := json.Unmarshal(req.Data, params)
+	if parseErr != nil {
+		sdkErr = sdk.ErrUnknownRequest(fmt.Sprintf("Incorrectly formatted request data - %s", parseErr.Error()))
+		return
+	}
+	return
+}
+
+// MustMarshal marshals a struct into JSON bytes
+func MustMarshal(v interface{}) (res []byte) {
+	res, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		panic("Could not marshal result to JSON")
+	}
+	return
+}
