88fecfcee916210cef700ff067e143aa829d0174
Shane Vitarana <shanev@gmail.com>
2018-12-06 12:56:09 -0800

    Seeding chain with challenged story

--- x/truapi/supported_msgs.go
+++ x/truapi/supported_msgs.go
@@ -2,11 +2,13 @@ package truapi
 
 import (
 	"github.com/TruStory/truchain/x/backing"
+	"github.com/TruStory/truchain/x/challenge"
 	"github.com/TruStory/truchain/x/chttp"
 	"github.com/TruStory/truchain/x/story"
 )
 
 var supported = chttp.MsgTypes{
-	"SubmitStoryMsg": story.SubmitStoryMsg{},
-	"BackStoryMsg":   backing.BackStoryMsg{},
+	"SubmitStoryMsg":     story.SubmitStoryMsg{},
+	"BackStoryMsg":       backing.BackStoryMsg{},
+	"CreateChallengeMsg": challenge.CreateChallengeMsg{},
 }
