457fc90a7777d8dcbc6e757648a0faa2c050e78c
Shane Vitarana <shanev@gmail.com>
2018-12-07 13:36:47 -0800

    Added backing story querier

---
+++ x/backing/queryable.go
@@ -0,0 +1,47 @@
+package backing
+
+import (
+	app "github.com/TruStory/truchain/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+)
+
+// query endpoints supported by the truchain Querier
+const (
+	QueryPath        = "backings"
+	QueryBackingByID = "id"
+)
+
+// QueryBackingByIDParams are params for stories by category queries
+type QueryBackingByIDParams struct {
+	ID int64
+}
+
+// NewQuerier returns a function that handles queries on the KVStore
+func NewQuerier(k ReadKeeper) sdk.Querier {
+	return func(ctx sdk.Context, path []string, req abci.RequestQuery) (res []byte, err sdk.Error) {
+		switch path[0] {
+		case QueryBackingByID:
+			return queryBackingByID(ctx, req, k)
+		default:
+			return nil, sdk.ErrUnknownRequest("Unknown truchain query endpoint")
+		}
+	}
+}
+
+// ============================================================================
+
+func queryBackingByID(ctx sdk.Context, req abci.RequestQuery, k ReadKeeper) (res []byte, err sdk.Error) {
+	params := QueryBackingByIDParams{}
+
+	if err = app.UnmarshalQueryParams(req, &params); err != nil {
+		return
+	}
+
+	backing, err := k.Backing(ctx, params.ID)
+	if err != nil {
+		return
+	}
+
+	return app.MustMarshal(backing), nil
+}
