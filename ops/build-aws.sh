3661fb61726915d72dddd91022aed0d61e52875b
Shane Vitarana <shanev@gmail.com>
2019-01-10 14:03:50 -0800

    Updated AWS deploy script

--- ops/build-aws.sh
+++ ops/build-aws.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+PATH=/home/ubuntu/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
 # go (no not like the language) to repo
 GOPATH=/home/ubuntu/go
 cd "${GOPATH}/src/github.com/TruStory/truchain"
@@ -14,9 +16,10 @@ make buidl
 # stop truchaind daemon
 sudo systemctl stop truchaind.service
 
-# copy binaries
+# copy files
 cp bin/truchaind $GOPATH/bin
 cp bin/trucli $GOPATH/bin
+cp .chain/bootstrap.csv ~/.truchaind/bootstrap.csv
 
 # start truchain daemon
 sudo systemctl start truchaind.service
