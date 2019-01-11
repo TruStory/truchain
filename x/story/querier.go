94b355dcf6dc57d5fdf2927659a402c4fdf37dd0
Shane Vitarana <shanev@gmail.com>
2019-01-11 11:13:53 -0800

    Added querier for params

--- x/story/querier.go
+++ x/story/querier.go
@@ -1,7 +1,12 @@
 package story
 
 import (
+	"time"
+
+	"github.com/TruStory/truchain/x/backing"
+
 	app "github.com/TruStory/truchain/types"
+	"github.com/cosmos/cosmos-sdk/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 )
@@ -76,4 +81,67 @@ func queryStories(ctx sdk.Context, _ abci.RequestQuery, k ReadKeeper) (res []byt
 	stories := k.Stories(ctx)
 
 	return app.MustMarshal(stories), nil
+}
+
+// const Parameters = {
+// 	MinStoryLength: 25,
+// 	MaxStoryLength: 350,
+// 	MinArgumentLength: 10,
+// 	MaxArgumentLength: 1000,
+// 	MinBackingDuration: 3,
+// 	MaxBackingDuration: 90,
+// 	MinChallengeStake: 10,
+// 	MaxBackingAmount: 100,
+// 	AddStoryStake: 10,
+//   };
+
+// Params holds default parameters for a story
+type Params struct {
+	BackingParams struct {
+		MinDuration time.Duration
+		MaxDuration time.Duration
+		MaxAmount   sdk.Coin
+	}
+	ChallengeParams struct {
+		MinChallengeStake int
+	}
+	MinStoryLength    int
+	MaxStoryLength    int
+	MinArgumentLength int
+	MaxArgumentLength int
+	StoryFee          sdk.Coin
+}
+
+func queryParams(
+	ctx sdk.Context, _ abci.RequestQuery, k ReadKeeper) (res []byte, err sdk.Error) {
+
+	params := Params{
+		BackingParams: struct {
+			MinDuration time.Duration
+			MaxDuration time.Duration
+			MaxAmount   types.Coin
+		}{
+			MinDuration: backing.DefaultMsgParams().MinPeriod,
+			MaxDuration: 0,
+			MaxAmount: types.Coin{
+				Denom:  "",
+				Amount: types.Int{},
+			},
+		},
+		ChallengeParams: struct {
+			MinChallengeStake int
+		}{
+			MinChallengeStake: 0,
+		},
+		MinStoryLength:    0,
+		MaxStoryLength:    0,
+		MinArgumentLength: 0,
+		MaxArgumentLength: 0,
+		StoryFee: types.Coin{
+			Denom:  "",
+			Amount: types.Int{},
+		},
+	}
+
+	return app.MustMarshal(params), nil
 }
