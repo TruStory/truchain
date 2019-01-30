9e192c9f57ab8a785589cc1cdf478b7af256dce9
Shane Vitarana <shanev@gmail.com>
2019-01-30 13:34:45 -0800

    Fixed not setting key

--- app/app.go
+++ app/app.go
@@ -208,6 +208,7 @@ func NewTruChain(logger log.Logger, db dbm.DB, options ...func(*bam.BaseApp)) *T
 	app.MountStoresIAVL(
 		app.keyAccount,
 		app.keyBacking,
+		app.keyBackingList,
 		app.keyCategory,
 		app.keyChallenge,
 		app.keyFee,
