bab9cfd14bec89c5915ac5eae1da33b5c16065e1
Ted Piotrowski <ted@trustory.io>
2019-02-04 16:44:20 -0800

    Send parameters required to calculate return rate to client

--- x/params/types.go
+++ x/params/types.go
@@ -4,23 +4,27 @@ import (
 	"time"
 
 	"github.com/TruStory/truchain/x/backing"
-	"github.com/TruStory/truchain/x/game"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // Params defines defaults for a story
 type Params struct {
-	BackingMinDuration time.Duration `json:"backing_min_duration,omitempty"`
-	BackingMaxDuration time.Duration `json:"backing_max_duration,omitempty"`
-	ChallengeMinStake  sdk.Int       `json:"challenge_min_stake,omitempty"`
+	BackingAmountWeight    string        `json:"backing_amount_weight,omitempty"`
+	BackingPeriodWeight    string        `json:"backing_period_weight,omitempty"`
+	BackingMinInterestRate string        `json:"backing_min_intereset_rate,omitempty"`
+	BackingMaxInterestRate string        `json:"backing_max_intereset_rate,omitempty"`
+	BackingMinDuration     time.Duration `json:"backing_min_duration,omitempty"`
+	BackingMaxDuration     time.Duration `json:"backing_max_duration,omitempty"`
 }
 
 // DefaultParams creates the default params
 func DefaultParams() Params {
 
 	return Params{
-		BackingMinDuration: backing.DefaultMsgParams().MinPeriod,
-		BackingMaxDuration: backing.DefaultMsgParams().MaxPeriod,
-		ChallengeMinStake:  game.DefaultParams().MinChallengeStake,
+		BackingAmountWeight:    backing.DefaultParams().AmountWeight.String(),
+		BackingPeriodWeight:    backing.DefaultParams().PeriodWeight.String(),
+		BackingMinInterestRate: backing.DefaultParams().MinInterestRate.String(),
+		BackingMaxInterestRate: backing.DefaultParams().MaxInterestRate.String(),
+		BackingMinDuration:     backing.DefaultMsgParams().MinPeriod,
+		BackingMaxDuration:     backing.DefaultMsgParams().MaxPeriod,
 	}
 }
