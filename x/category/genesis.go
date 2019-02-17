2d35182b98968a5c4992a062d7652b10f54fd886
Shane Vitarana <shanev@gmail.com>
2019-02-17 09:58:06 -0800

    Added default categories to chain init

--- x/category/genesis.go
+++ x/category/genesis.go
@@ -2,6 +2,14 @@ package category
 
 import sdk "github.com/cosmos/cosmos-sdk/types"
 
+// DefaultCategories for tests and chain init
+func DefaultCategories() []Category {
+	return []Category{
+		{Title: "Cryptocurrency", Slug: "crypto"},
+		{Title: "Memes", Slug: "meme"},
+	}
+}
+
 // InitGenesis loads initial categories from the genesis file
 func InitGenesis(ctx sdk.Context, categoryKeeper WriteKeeper, categories []Category) {
 	for _, cat := range categories {
