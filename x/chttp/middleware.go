9e2a8dd7f775bc58541d4abe3b09c447a175a3f3
Ted Piotrowski <ted@trustory.io>
2019-03-06 15:56:05 -0800

    Add CORS middleware to give website access to truapi

--- x/chttp/middleware.go
+++ x/chttp/middleware.go
@@ -11,3 +11,16 @@ func JSONResponseMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+// CORSMiddleware is an HTTP-handling middleware that adds `Access-Control-Allow-Origin: *` to the response.
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		if r.Method == "OPTIONS" {
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
