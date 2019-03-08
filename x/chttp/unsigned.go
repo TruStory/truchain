76ad28ccfb946b6a9341551d141768e140cba977
Mohit Mamoria <mohit.mamoria@gmail.com>
2019-03-08 23:23:43 +0530

    Made the unsigned route

---
+++ x/chttp/unsigned.go
@@ -0,0 +1,55 @@
+package chttp
+
+import (
+	"encoding/hex"
+
+	"github.com/TruStory/truchain/x/db"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	secp "github.com/tendermint/tendermint/crypto/secp256k1"
+	tcmn "github.com/tendermint/tendermint/libs/common"
+)
+
+// UnsignedRequest represents a JSON request body from a user wishing to broadcast a transaction that they've signed locally
+type UnsignedRequest struct {
+	MsgTypes []string      `json:"msg_types"`
+	Tx       tcmn.HexBytes `json:"tx"`
+	TxHash   string        `json:"tx_hash"`
+}
+
+// NewUnsignedStdTx parses an `UnsignedRequest` into an `auth.StdTx`
+func (a *API) NewUnsignedStdTx(r UnsignedRequest, keyPair db.KeyPair) (auth.StdTx, error) {
+
+	txHashBytes, err := hex.DecodeString(r.TxHash)
+	if err != nil {
+		panic(err)
+	}
+
+	// Signing the hash
+	privateKey := GetPrivateKeyObject(keyPair)
+
+	signature, err := privateKey.Sign(txHashBytes)
+
+	presignedRequest := &PresignedRequest{
+		MsgTypes:   r.MsgTypes,
+		Tx:         r.Tx,
+		PubKeyAlgo: "secp256k1",
+		PubKey:     privateKey.PubKey().Bytes(),
+		Signature:  signature,
+	}
+
+	return a.NewPresignedStdTx(*presignedRequest)
+}
+
+// GetPrivateKeyObject returns the secp's object encapsulating the private key
+func GetPrivateKeyObject(keyPair db.KeyPair) secp.PrivKeySecp256k1 {
+	privateKey32Bytes := [32]byte{}
+	privateKeyBytes, err := hex.DecodeString(keyPair.PrivateKey)
+	if err != nil {
+		panic(err)
+	}
+
+	// make it of the fixed length of 32 bytes
+	copy(privateKey32Bytes[:], privateKeyBytes)
+
+	return secp.PrivKeySecp256k1(privateKey32Bytes)
+}
---
+++ x/truapi/handle_unsigned.go
@@ -0,0 +1,92 @@
+package truapi
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/TruStory/truchain/x/chttp"
+	"github.com/gorilla/securecookie"
+)
+
+// HandleUnsigned takes a `HandleUnsignedRequest` and returns a `HandleUnsignedResponse`
+func (ta *TruAPI) HandleUnsigned(r *http.Request) chttp.Response {
+	txr := new(chttp.UnsignedRequest)
+	jsonBytes, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		return chttp.SimpleErrorResponse(500, err)
+	}
+
+	err = json.Unmarshal(jsonBytes, txr)
+
+	if err != nil {
+		return chttp.SimpleErrorResponse(400, err)
+	}
+
+	// Get the user context
+	truUser := GetTruUserFromCookie(r)
+	twitterProfileID, err := strconv.ParseInt(truUser["twitter-profile-id"], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	// Fetch keypair of the user
+	keyPair, err := ta.DBClient.KeyPairByTwitterProfileID(twitterProfileID)
+	if err != nil {
+		return chttp.SimpleErrorResponse(400, err)
+	}
+	if keyPair.ID == 0 {
+		// keypair doesn't exist
+		return chttp.SimpleErrorResponse(400, errors.New("keypair does not exist on the server"))
+	}
+
+	tx, err := ta.NewUnsignedStdTx(*txr, keyPair)
+
+	if err != nil {
+		fmt.Println("Error decoding tx: ", err)
+		return chttp.SimpleErrorResponse(400, err)
+	}
+
+	res, err := ta.DeliverPresigned(tx)
+
+	if err != nil {
+		return chttp.SimpleErrorResponse(400, err)
+	}
+
+	resBytes, _ := json.Marshal(res)
+
+	return chttp.SimpleResponse(200, resBytes)
+}
+
+// GetTruUserFromCookie gets the user context from the request's cookie
+func GetTruUserFromCookie(r *http.Request) map[string]string {
+	truUser, err := r.Cookie("tru-user")
+	if err != nil {
+		panic(err)
+	}
+
+	hashKey, err := hex.DecodeString(os.Getenv("COOKIE_HASH_KEY"))
+	if err != nil {
+		panic(err)
+	}
+	blockKey, err := hex.DecodeString(os.Getenv("COOKIE_ENCRYPT_KEY"))
+	if err != nil {
+		panic(err)
+	}
+	var s = securecookie.New(hashKey, blockKey)
+
+	decodedTruUser := make(map[string]string)
+	err = s.Decode("tru-user", truUser.Value, &decodedTruUser)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return decodedTruUser
+}
