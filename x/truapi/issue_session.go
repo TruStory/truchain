d11afd7ff381443c023851498fa6cdd56ac093d0
Mohit Mamoria <mohit.mamoria@gmail.com>
2019-03-28 14:58:55 +0530

    handled the oauth denial case

--- x/truapi/issue_session.go
+++ x/truapi/issue_session.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/TruStory/truchain/x/db"
 	"github.com/TruStory/truchain/x/truapi/cookies"
+	"github.com/dghubble/gologin"
 	"github.com/dghubble/gologin/twitter"
 )
 
@@ -45,6 +46,29 @@ func IssueSession(ta *TruAPI) http.Handler {
 		}
 		http.SetCookie(w, cookie)
 		http.Redirect(w, req, os.Getenv("AUTH_LOGIN_REDIR"), http.StatusFound)
+	}
+	return http.HandlerFunc(fn)
+}
+
+// HandleOAuthFailure handles the failed oAuth requests gracefully
+func HandleOAuthFailure(ta *TruAPI) http.Handler {
+	fn := func(w http.ResponseWriter, req *http.Request) {
+		// if the authorization was purposefully denied by the user
+		if req.FormValue("denied") != "" {
+			http.Redirect(w, req, os.Getenv("AUTH_LOGIN_DENIED_REDIR"), http.StatusFound)
+			return
+		}
+
+		// if any other error
+		ctx := req.Context()
+		err := gologin.ErrorFromContext(ctx)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		// should be unreachable, ErrorFromContext always returns some non-nil error
+		http.Error(w, "", http.StatusInternalServerError)
 	}
 	return http.HandlerFunc(fn)
 }
