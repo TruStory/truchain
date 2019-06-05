4c0f1ac7660909ec5dc3a99101983506f8f61da2
jhernandezb <contact@jhernandez.me>
2019-06-05 08:22:39 -0600

    Fixed restore default params for expiration module

--- x/expiration/params.go
+++ x/expiration/params.go
@@ -7,18 +7,40 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/params"
 )
 
+// store keys for expiration params
+var (
+	KeyAmountWeight    = []byte("amountWeight")
+	KeyPeriodWeight    = []byte("periodWeight")
+	KeyMinInterestRate = []byte("minInterestRate")
+	KeyMaxInterestRate = []byte("maxInterestRate")
+)
+
 // Params holds parameters for a story
 type Params struct {
+	AmountWeight    sdk.Dec `json:"amount_weight"`
+	PeriodWeight    sdk.Dec `json:"period_weight"`
+	MinInterestRate sdk.Dec `json:"min_interest_rate"`
+	MaxInterestRate sdk.Dec `json:"max_interest_rate"`
 }
 
 // DefaultParams is the story params for testing
 func DefaultParams() Params {
-	return Params{}
+	return Params{
+		AmountWeight:    sdk.NewDecWithPrec(333, 3), // 33.3%
+		PeriodWeight:    sdk.NewDecWithPrec(667, 3), // 66.7%
+		MinInterestRate: sdk.ZeroDec(),              // 0%
+		MaxInterestRate: sdk.NewDecWithPrec(10, 2),  // 10%
+	}
 }
 
 // ParamSetPairs implements params.ParamSet
 func (p *Params) ParamSetPairs() params.ParamSetPairs {
-	return params.ParamSetPairs{}
+	return params.ParamSetPairs{
+		{Key: KeyAmountWeight, Value: &p.AmountWeight},
+		{Key: KeyPeriodWeight, Value: &p.PeriodWeight},
+		{Key: KeyMinInterestRate, Value: &p.MinInterestRate},
+		{Key: KeyMaxInterestRate, Value: &p.MaxInterestRate},
+	}
 }
 
 // ParamKeyTable for story module
