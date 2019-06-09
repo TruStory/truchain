3e8d0cc9c5b59f026aec39214c52fcf0c41f26f5
Mohit Mamoria <mohit.mamoria@gmail.com>
2019-06-09 23:23:58 +0530

    added params to genesis

--- x/auth/genesis.go
+++ x/auth/genesis.go
@@ -1,35 +1,43 @@
 package auth
 
-import sdk "github.com/cosmos/cosmos-sdk/types"
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
 
 // GenesisState defines genesis data for the module
 type GenesisState struct {
-	Auths []Auth `json:"auths"`
+	Params Params `json:"params"`
 }
 
 // NewGenesisState creates a new genesis state.
-func NewGenesisState() GenesisState {
+func NewGenesisState(params Params) GenesisState {
 	return GenesisState{
-		Auths: nil,
+		Params: params,
 	}
 }
 
 // DefaultGenesisState returns a default genesis state
-func DefaultGenesisState() GenesisState { return NewGenesisState() }
+func DefaultGenesisState() GenesisState { return NewGenesisState(DefaultParams()) }
 
 // InitGenesis initializes story state from genesis file
 func InitGenesis(ctx sdk.Context, keeper Keeper, data GenesisState) {
+	keeper.SetParams(ctx, data.Params)
 }
 
 // ExportGenesis exports the genesis state
 func ExportGenesis(ctx sdk.Context, keeper Keeper) GenesisState {
 	return GenesisState{
-//		Auths: keeper.Auths(ctx),
+		Params: keeper.GetParams(ctx),
 	}
 }
 
 // ValidateGenesis validates the genesis state data
 func ValidateGenesis(data GenesisState) error {
+	if data.Params.MaxSlashCount < 1 {
+		return fmt.Errorf("Param: MaxSlashCount, must have a positive value")
+	}
 
 	return nil
 }
--- x/auth/types.go
+++ x/auth/types.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkAuth "github.com/cosmos/cosmos-sdk/x/auth"
 )
 
 // Defines auth module constants
@@ -12,9 +13,23 @@ const (
 	QuerierRoute = ModuleName
 )
 
-// Auth stores data about a auth
-type Auth struct {
-	ID              int64
-	Creator         sdk.AccAddress
-	CreatedTime     time.Time
+// EarnedCoin is a representation of a coin associated with a community, or "earned trustake".
+type EarnedCoin struct {
+	sdk.Coin
+
+	CommunityID int64
+}
+
+// EarnedCoins is a collection of EarnedCoins
+type EarnedCoins []EarnedCoin
+
+// AppAccount is the main account for a TruStory user.
+type AppAccount struct {
+	sdkAuth.BaseAccount
+
+	EarnedStake EarnedCoins
+	SlashCount  int
+	IsJailed    bool
+	JailEndTime time.Time
+	CreatedTime time.Time
 }
