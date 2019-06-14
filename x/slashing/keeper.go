6b6a46f2c4412180c8e341b01c24553ebfdbeba8
Mohit Mamoria <mohit.mamoria@gmail.com>
2019-06-14 16:12:36 +0530

    Added params

--- x/slashing/keeper.go
+++ x/slashing/keeper.go
@@ -1,4 +1,25 @@
 package slashing
 
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/params"
+	"github.com/shanev/cosmos-record-keeper/recordkeeper"
+)
+
 // Keeper is the model object for the package slashing module
-type Keeper struct {}
+type Keeper struct {
+	recordkeeper.RecordKeeper
+
+	codec      *codec.Codec
+	paramStore params.Subspace
+}
+
+// NewKeeper creates a new keeper of the slashing Keeper
+func NewKeeper(storeKey sdk.StoreKey, paramStore params.Subspace, codec *codec.Codec) Keeper {
+	return Keeper{
+		recordkeeper.NewRecordKeeper(storeKey, codec),
+		codec,
+		paramStore.WithKeyTable(ParamKeyTable()),
+	}
+}
---
+++ x/slashing/params.go
@@ -0,0 +1,68 @@
+package slashing
+
+import (
+	"fmt"
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/params"
+)
+
+// Keys for params
+var (
+	KeyMaxStakeSlashCount = []byte("maxStakeSlashCount")
+	KeySlashMagnitude     = []byte("slashMagnitude")
+	KeySlashMinStake      = []byte("slashMinStake")
+	KeySlashAdmins        = []byte("slashAdmins")
+	KeyJailTime           = []byte("jailTime")
+)
+
+// Params holds parameters for Slashing
+type Params struct {
+	MaxStakeSlashCount int              `json:"max_slash_stake_count"`
+	SlashMagnitude     sdk.Dec          `json:"slash_magnitude"`
+	SlashMinStake      sdk.Coin         `json:"slash_min_stake"`
+	SlashAdmins        []sdk.AccAddress `json:"slash_admins"`
+	JailTime           time.Duration    `json:"jail_time"`
+}
+
+// DefaultParams is the Slashing params for testing
+func DefaultParams() Params {
+	return Params{
+		MaxStakeSlashCount: 50,
+		SlashMagnitude:     sdk.NewDec(3),
+		SlashMinStake:      sdk.NewCoin("trustake", sdk.NewInt(50)),
+		SlashAdmins:        []sdk.AccAddress{},
+		JailTime:           time.Duration((7 * 24) * time.Hour),
+	}
+}
+
+// ParamSetPairs implements params.ParamSet
+func (p *Params) ParamSetPairs() params.ParamSetPairs {
+	return params.ParamSetPairs{
+		{Key: KeyMaxStakeSlashCount, Value: &p.MaxStakeSlashCount},
+		{Key: KeySlashMagnitude, Value: &p.SlashMagnitude},
+		{Key: KeySlashMinStake, Value: &p.SlashMinStake},
+		{Key: KeySlashAdmins, Value: &p.SlashAdmins},
+		{Key: KeyJailTime, Value: &p.JailTime},
+	}
+}
+
+// ParamKeyTable for slashing module
+func ParamKeyTable() params.KeyTable {
+	return params.NewKeyTable().RegisterParamSet(&Params{})
+}
+
+// GetParams gets the genesis params for the slashing
+func (k Keeper) GetParams(ctx sdk.Context) Params {
+	var paramSet Params
+	k.paramStore.GetParamSet(ctx, &paramSet)
+	return paramSet
+}
+
+// SetParams sets the params for the slashing
+func (k Keeper) SetParams(ctx sdk.Context, params Params) {
+	logger := ctx.Logger().With("module", ModuleName)
+	k.paramStore.SetParamSet(ctx, &params)
+	logger.Info(fmt.Sprintf("Loaded slashing params: %+v", params))
+}
