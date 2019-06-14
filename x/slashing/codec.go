a7d672da914cf5b12f6cf68129296281f063f578
Mohit Mamoria <mohit.mamoria@gmail.com>
2019-06-14 22:47:22 +0530

    Filled the RegisterCodec

--- x/slashing/codec.go
+++ x/slashing/codec.go
@@ -6,5 +6,5 @@ var moduleCodec = codec.New()
 
 // RegisterCodec registers all the necessary types and interfaces for the module
 func RegisterCodec(cdc *codec.Codec) {
-    
+    cdc.RegisterConcrete(MsgSlashArgument{}, "slashing/MsgSlashArgument", nil)
 }
