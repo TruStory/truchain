40053f6aa2bb18799663f7bd90579f92f65a1bd7
Mohit Mamoria <mohit.mamoria@gmail.com>
2019-06-14 17:51:43 +0530

    added querier and its tests

--- x/slashing/querier.go
+++ x/slashing/querier.go
@@ -1,13 +1,70 @@
 package slashing
 
 import (
-	abci "github.com/tendermint/tendermint/abci/types"
+	"encoding/json"
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
 )
+
+// query endpoints supported by the truchain Querier
+const (
+	QuerySlash   = "id"
+	QuerySlashes = "all"
+)
+
+// QuerySlashParams are params for querying slashes by id queries
+type QuerySlashParams struct {
+	ID uint64
+}
 
 // NewQuerier creates a new querier
 func NewQuerier(keeper Keeper) sdk.Querier {
-	return func(ctx sdk.Context, path []string, req abci.RequestQuery) ([]byte, sdk.Error) {
-		return nil, sdk.ErrUnknownRequest("Unknown package slashing query endpoint")
+	return func(ctx sdk.Context, path []string, request abci.RequestQuery) ([]byte, sdk.Error) {
+		switch path[0] {
+		case QuerySlash:
+			return querySlash(ctx, request, keeper)
+		case QuerySlashes:
+			return querySlashes(ctx, keeper)
+		default:
+			return nil, sdk.ErrUnknownRequest(fmt.Sprintf("Unknown truchain query endpoint: slashing/%s", path[0]))
+		}
 	}
+}
+
+func querySlash(ctx sdk.Context, request abci.RequestQuery, k Keeper) (result []byte, err sdk.Error) {
+	params := QuerySlashParams{}
+	if err = unmarshalQueryParams(request, &params); err != nil {
+		return
+	}
+
+	slash, err := k.Slash(ctx, params.ID)
+	if err != nil {
+		return
+	}
+
+	return mustMarshal(slash), nil
+}
+
+func querySlashes(ctx sdk.Context, k Keeper) (result []byte, err sdk.Error) {
+	slashes := k.Slashes(ctx)
+	return mustMarshal(slashes), nil
+}
+
+func unmarshalQueryParams(request abci.RequestQuery, params interface{}) (sdkErr sdk.Error) {
+	err := json.Unmarshal(request.Data, params)
+	if err != nil {
+		sdkErr = sdk.ErrUnknownRequest(fmt.Sprintf("Incorrectly formatted request data - %s", err.Error()))
+		return
+	}
+	return
+}
+
+func mustMarshal(v interface{}) (result []byte) {
+	result, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		panic("Could not marshal result to JSON")
+	}
+	return
 }
---
+++ x/slashing/querier_test.go
@@ -0,0 +1,77 @@
+package slashing
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	abci "github.com/tendermint/tendermint/abci/types"
+)
+
+func TestQuerySlash_Success(t *testing.T) {
+	ctx, keeper := mockDB()
+
+	stakeID := uint64(1)
+	creator := DefaultParams().SlashAdmins[0]
+	createdSlash, err := keeper.NewSlash(ctx, stakeID, creator)
+	assert.Nil(t, err)
+
+	params, jsonErr := json.Marshal(QuerySlashParams{
+		ID: 1,
+	})
+	assert.Nil(t, jsonErr)
+
+	query := abci.RequestQuery{
+		Path: "/custom/slashing/id",
+		Data: params,
+	}
+
+	result, sdkErr := querySlash(ctx, query, keeper)
+	assert.Nil(t, sdkErr)
+
+	var returnedSlash Slash
+	jsonErr = json.Unmarshal(result, &returnedSlash)
+	assert.Nil(t, jsonErr)
+	assert.Equal(t, returnedSlash, createdSlash)
+}
+
+func TestQuerySlash_ErrNotFound(t *testing.T) {
+	ctx, keeper := mockDB()
+
+	params, jsonErr := json.Marshal(QuerySlashParams{
+		ID: 1,
+	})
+	assert.Nil(t, jsonErr)
+
+	query := abci.RequestQuery{
+		Path: "/custom/slashing/id",
+		Data: params,
+	}
+
+	_, sdkErr := querySlash(ctx, query, keeper)
+	assert.NotNil(t, sdkErr)
+	assert.Equal(t, ErrSlashNotFound(1).Code(), sdkErr.Code())
+}
+
+func TestQuerySlashes_Success(t *testing.T) {
+	ctx, keeper := mockDB()
+
+	stakeID := uint64(1)
+	creator := DefaultParams().SlashAdmins[0]
+	first, err := keeper.NewSlash(ctx, stakeID, creator)
+	assert.Nil(t, err)
+
+	stakeID2 := uint64(2)
+	another, err := keeper.NewSlash(ctx, stakeID2, creator)
+	assert.Nil(t, err)
+
+	result, sdkErr := querySlashes(ctx, keeper)
+	assert.Nil(t, sdkErr)
+
+	var all []Slash
+	jsonErr := json.Unmarshal(result, &all)
+	assert.Nil(t, jsonErr)
+	assert.Len(t, all, 2)
+	assert.Equal(t, all[0], first)
+	assert.Equal(t, all[1], another)
+}
