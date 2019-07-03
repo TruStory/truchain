b23d268cc0323972a494f61b293412873514c839
Shane Vitarana <shanev@gmail.com>
2019-07-03 13:06:11 -0700

    Added accounts by addresses query

--- x/account/types.go
+++ x/account/types.go
@@ -1,6 +1,7 @@
 package account
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/cosmos/cosmos-sdk/x/auth"
@@ -24,6 +25,16 @@ type AppAccount struct {
 	IsJailed    bool      `json:"is_jailed"`
 	JailEndTime time.Time `json:"jail_end_time"`
 	CreatedTime time.Time `json:"created_time"`
+}
+
+// String implements fmt.Stringer
+func (acc AppAccount) String() string {
+	return fmt.Sprintf(`%s
+  SlashCount:        %d
+  IsJailed:          %t
+  JailEndTime:       %s
+  CreatedTime:       %s`,
+		acc.BaseAccount.String(), acc.SlashCount, acc.IsJailed, acc.JailEndTime.String(), acc.CreatedTime.String())
 }
 
 // AppAccounts is a slice of AppAccounts
