ad6b0dd5e815183b3a5b1e794ba1021a066936c6
jhernandezb <contact@jhernandez.me>
2019-07-19 10:52:40 -0600

    add unjailed addresses to end block

--- x/account/abci.go
+++ x/account/abci.go
@@ -2,7 +2,11 @@ package account
 
 import (
 	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	app "github.com/TruStory/truchain/types"
+	"github.com/TruStory/truchain/x/staking/tags"
 )
 
 // EndBlocker called every block, process expiring stakes
@@ -11,13 +15,28 @@ func EndBlocker(ctx sdk.Context, keeper Keeper) sdk.Tags {
 	if err != nil {
 		panic(err)
 	}
+	unjailed := make([]string, 0)
 	for _, acct := range toUnjail {
 		err = keeper.UnJail(ctx, acct.GetAddress())
 		if err != nil {
 			panic(err)
 		}
+		unjailed = append(unjailed, acct.GetAddress().String())
 		logger(ctx).Info(fmt.Sprintf("Unjailed %s", acct.String()))
 	}
-
+	if len(unjailed) == 0 {
+		return sdk.EmptyTags()
+	}
+	b, jsonErr := keeper.codec.MarshalJSON(unjailed)
+	if jsonErr != nil {
+		panic(jsonErr)
+	}
+	return append(app.PushTag,
+		sdk.NewTags(
+			tags.Category, tags.TxCategory,
+			tags.Action, tags.ActionInterestRewardPaid,
+			tags.ExpiredStakes, b,
+		)...,
+	)
 	return sdk.EmptyTags()
 }
