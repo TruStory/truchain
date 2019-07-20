5d5aab62dacc7c87c78f45d1045f778f4302161b
Shane Vitarana <shanev@gmail.com>
2019-07-20 14:38:40 -0700

    Removed Timestamp

--- types/types.go
+++ types/types.go
@@ -1,11 +1,8 @@
 package types
 
 import (
-	"time"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth"
-	abci "github.com/tendermint/tendermint/abci/types"
 )
 
 const (
@@ -82,32 +79,6 @@ type CompletedStory struct {
 // completed stories in a new Block.
 type CompletedStoriesNotificationResult struct {
 	Stories []CompletedStory `json:"stories"`
-}
-
-// Timestamp records the timestamp for a type
-type Timestamp struct {
-	CreatedBlock int64     `json:"created_block,omitempty"`
-	CreatedTime  time.Time `json:"created_time,omitempty"`
-	UpdatedBlock int64     `json:"updated_block,omitempty"`
-	UpdatedTime  time.Time `json:"updated_time,omitempty"`
-}
-
-// NewTimestamp creates a new default Timestamp
-func NewTimestamp(blockHeader abci.Header) Timestamp {
-	return Timestamp{
-		blockHeader.Height,
-		blockHeader.Time,
-		blockHeader.Height,
-		blockHeader.Time,
-	}
-}
-
-// Update updates an existing Timestamp and returns a new one
-func (t Timestamp) Update(blockHeader abci.Header) Timestamp {
-	t.UpdatedBlock = blockHeader.Height
-	t.UpdatedTime = blockHeader.Time
-
-	return t
 }
 
 // StakeReward represents the amount of stake earned by an user.
