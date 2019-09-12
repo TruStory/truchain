fa402736242248aa56bca175fb2d89522245b675
Shane Vitarana <shanev@gmail.com>
2019-09-11 23:43:19 -0700

    Distribute inflation in end blocker

--- x/account/abci.go
+++ x/account/abci.go
@@ -3,18 +3,25 @@ package account
 import (
 	"fmt"
 
+	app "github.com/TruStory/truchain/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth"
 )
 
 // EndBlocker called every block, process expiring stakes
 func EndBlocker(ctx sdk.Context, keeper Keeper) {
-	toUnjail, err := keeper.JailedAccountsBefore(ctx, ctx.BlockHeader().Time)
+	keeper.distributeInflation(ctx)
+	keeper.unjailAccounts(ctx)
+}
+
+func (k Keeper) unjailAccounts(ctx sdk.Context) {
+	toUnjail, err := k.JailedAccountsBefore(ctx, ctx.BlockHeader().Time)
 	if err != nil {
 		panic(err)
 	}
 
 	for _, acct := range toUnjail {
-		err = keeper.UnJail(ctx, acct.PrimaryAddress())
+		err = k.UnJail(ctx, acct.PrimaryAddress())
 		if err != nil {
 			panic(err)
 		}
@@ -26,18 +33,22 @@ func EndBlocker(ctx sdk.Context, keeper Keeper) {
 			),
 		)
 
-		keeper.Logger(ctx).Info(fmt.Sprintf("Unjailed %s", acct.String()))
+		k.Logger(ctx).Info(fmt.Sprintf("Unjailed %s", acct.String()))
 	}
 }
 
-func distributeInflation(ctx sdk.Context, keeper Keeper) sdk.Error {
-	//userGrowthAllocation := sdk.NewDecWithPrec(800, 3)
-	//
-	//amount := sdk.NewCoins(sdk.NewInt64Coin("tru", 23423))
-	//err := keeper.supplyKeeper.SendCoinsFromModuleToModule(ctx, auth.FeeCollectorName, UserGrowthPoolName, amount)
-	//if err != nil {
-	//	return err
-	//}
+func (k Keeper) distributeInflation(ctx sdk.Context) {
+	// TODO: take this from params
+	userGrowthAllocation := sdk.NewDecWithPrec(800, 3)
 
-	return nil
+	acc := k.supplyKeeper.GetModuleAccount(ctx, auth.FeeCollectorName)
+	userInflation := acc.GetCoins().AmountOf(app.StakeDenom)
+	userInflationDec := sdk.NewDecFromIntWithPrec(userInflation, 3)
+	userGrowthAmount := userInflationDec.Mul(userGrowthAllocation).RoundInt()
+	userGrowthCoins := sdk.NewCoins(sdk.NewCoin(app.StakeDenom, userGrowthAmount))
+
+	err := k.supplyKeeper.SendCoinsFromModuleToModule(ctx, auth.FeeCollectorName, UserGrowthPoolName, userGrowthCoins)
+	if err != nil {
+		panic(err)
+	}
 }
