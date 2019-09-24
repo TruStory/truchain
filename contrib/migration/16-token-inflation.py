798ea622d0ebb3b3d4e29f9f64844f77f2c6cd8b
Shane Vitarana <shanev@gmail.com>
2019-09-23 18:03:07 -0700

    Added other module accounts to migration script

--- contrib/migration/16-token-inflation.py
+++ contrib/migration/16-token-inflation.py
@@ -5,7 +5,71 @@ import lib
 def process_genesis(genesis, parsed_args):
     genesis['app_state']['distribution']['community_tax'] = '0.800000000000000000'
 
+    # Add modules accounts that hold inflation fund pools
 
+    # This is the account that holds the total inflation from each block.
+    # The naming is bit of a misnomer due to historical reasons with Cosmos.
+    # The address of the account is derived from the name "fee_collector", which
+    # the mint modules uses. Thus it cannot be changed.
+    feeCollectorAccnt = {
+        'address': 'cosmos17xpfvakm2amg962yls6f84z3kell8c5lserqta',
+        'coins': [{'denom': 'tru', 'amount': '0'}],
+        'sequence_number': '0',
+        'account_number':'0',
+        'original_vesting': [],
+        'delegated_free': [],
+        'delegated_vesting': [],
+        'start_time': '0',
+        'end_time': '0',
+        'module_name': 'fee_collector',
+        'module_permissions': [],
+    }
+    genesis['app_state']['accounts'].append(feeCollectorAccnt)
+
+    userRewardPoolAccnt = {
+        'address': 'cosmos1ed82m7snyk8mux8xxpwygvtyq633a4k43rfp8l',
+        'coins': [{'denom': 'tru', 'amount': '2500000'}],
+        'sequence_number': '0',
+        'account_number':'0',
+        'original_vesting': [],
+        'delegated_free': [],
+        'delegated_vesting': [],
+        'start_time': '0',
+        'end_time': '0',
+        'module_name': 'user_reward_tokens_pool',
+        'module_permissions': [],
+    }
+    genesis['app_state']['accounts'].append(userRewardPoolAccnt)
+
+    userGrowthPoolAccnt = {
+        'address': 'cosmos1f7x5wx3adh6klcurmd8n36etx4elgu9d4wkys3',
+        'coins': [{'denom': 'tru', 'amount': '2500000'}],
+        'sequence_number': '0',
+        'account_number':'0',
+        'original_vesting': [],
+        'delegated_free': [],
+        'delegated_vesting': [],
+        'start_time': '0',
+        'end_time': '0',
+        'module_name': 'user_growth_tokens_pool',
+        'module_permissions': [],
+    }
+    genesis['app_state']['accounts'].append(userGrowthPoolAccnt)
+
+    stakeholderPoolAccnt = {
+        'address': 'cosmos1m9rhdryf059x684um3pa9n30tsdxuww84pxemz',
+        'coins': [{'denom': 'tru', 'amount': '0'}],
+        'sequence_number': '0',
+        'account_number':'0',
+        'original_vesting': [],
+        'delegated_free': [],
+        'delegated_vesting': [],
+        'start_time': '0',
+        'end_time': '0',
+        'module_name': 'stakeholder_tokens_pool',
+        'module_permissions': [],
+    }
+    genesis['app_state']['accounts'].append(stakeholderPoolAccnt)
 
     return genesis
 
