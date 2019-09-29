a9dee7e36dbbcd93d5ee706ea5b32577fe33ccf7
Shane Vitarana <shanev@gmail.com>
2019-09-28 21:36:36 -0700

    Added slashing migration

--- contrib/migration/16-token-inflation.py
+++ contrib/migration/16-token-inflation.py
@@ -43,6 +43,19 @@ def process_genesis(genesis, parsed_args):
         }
     }
 
+    genesis['app_state']['slashing'] = {
+        'params': {
+            'max_evidence_age': '120000000000',
+            'signed_blocks_window': '100',
+            'min_signed_per_window': '0.500000000000000000',
+            'downtime_jail_duration': '600000000000',
+            'slash_fraction_double_sign': '0.050000000000000000',
+            'slash_fraction_downtime': '0.010000000000000000'
+        },
+        'signing_infos': {},
+        'missed_blocks': {}
+    }
+
     # Add modules accounts that hold inflation fund pools
 
     # This is the account that holds the total inflation from each block.
