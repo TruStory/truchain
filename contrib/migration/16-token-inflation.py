7525cf0afb754a387fe86f14ce8b5049f39015ce
Shane Vitarana <shanev@gmail.com>
2019-09-30 17:11:32 -0700

    Removed module accounts because they are created automatically

--- contrib/migration/16-token-inflation.py
+++ contrib/migration/16-token-inflation.py
@@ -4,24 +4,7 @@ import lib
 
 def process_genesis(genesis, parsed_args):
     genesis['app_state']['distribution']['community_tax'] = '0.800000000000000000'
-    # this doesn't have to change, already in genesis
-    # "distribution": {
-    #                     "fee_pool": {
-    #                         "community_pool": []
-    #                     },
-    #                     "community_tax": "0.020000000000000000",
-    #                     "base_proposer_reward": "0.010000000000000000",
-    #                     "bonus_proposer_reward": "0.040000000000000000",
-    #                     "withdraw_addr_enabled": true,
-    #                     "delegator_withdraw_infos": [],
-    #                     "previous_proposer": "",
-    #                     "outstanding_rewards": [],
-    #                     "validator_accumulated_commissions": [],
-    #                     "validator_historical_rewards": [],
-    #                     "validator_current_rewards": [],
-    #                     "delegator_starting_infos": [],
-    #                     "validator_slash_events": []
-    #                 },
+    # NOTE: the rest of distribution should already be in the db by this point
 
     # staking from init genesis
     # "staking": {
@@ -39,8 +22,6 @@ def process_genesis(genesis, parsed_args):
     #                "redelegations": null,
     #                "exported": false
     #            },
-
-
 
     genesis['app_state']['supply'] = {
         'supply': [
@@ -94,78 +75,12 @@ def process_genesis(genesis, parsed_args):
         'gentx': None,
     }
 
-    # Add modules accounts that hold inflation fund pools
-
     # module account allocations
     genesis['app_state']['account']['params']['user_growth_allocation'] = '0.200000000000000000'
     genesis['app_state']['account']['params']['stakeholder_allocation'] = '0.200000000000000000'
     genesis['app_state']['trustaking']['params']['user_reward_allocation'] = '0.200000000000000000'
 
-    # This is the account that holds the total inflation from each block.
-    # The naming is bit of a misnomer due to historical reasons with Cosmos.
-    # The address of the account is derived from the name "fee_collector", which
-    # the mint modules uses. Thus it cannot be changed.
-
-    # TODO: replace this with distribution?
-    feeCollectorAccnt = {
-        'address': 'cosmos17xpfvakm2amg962yls6f84z3kell8c5lserqta',
-        'coins': [{'denom': 'tru', 'amount': '0'}],
-        'sequence_number': '0',
-        'account_number':'0',
-        'original_vesting': [],
-        'delegated_free': [],
-        'delegated_vesting': [],
-        'start_time': '0',
-        'end_time': '0',
-        'module_name': 'fee_collector',
-        'module_permissions': [],
-    }
-    genesis['app_state']['accounts'].append(feeCollectorAccnt)
-
-    userRewardPoolAccnt = {
-        'address': 'cosmos1ed82m7snyk8mux8xxpwygvtyq633a4k43rfp8l',
-        'coins': [{'denom': 'tru', 'amount': '2500000'}],
-        'sequence_number': '0',
-        'account_number':'0',
-        'original_vesting': [],
-        'delegated_free': [],
-        'delegated_vesting': [],
-        'start_time': '0',
-        'end_time': '0',
-        'module_name': 'user_reward_tokens_pool',
-        'module_permissions': [],
-    }
-    genesis['app_state']['accounts'].append(userRewardPoolAccnt)
-
-    userGrowthPoolAccnt = {
-        'address': 'cosmos1f7x5wx3adh6klcurmd8n36etx4elgu9d4wkys3',
-        'coins': [{'denom': 'tru', 'amount': '2500000'}],
-        'sequence_number': '0',
-        'account_number':'0',
-        'original_vesting': [],
-        'delegated_free': [],
-        'delegated_vesting': [],
-        'start_time': '0',
-        'end_time': '0',
-        'module_name': 'user_growth_tokens_pool',
-        'module_permissions': [],
-    }
-    genesis['app_state']['accounts'].append(userGrowthPoolAccnt)
-
-    stakeholderPoolAccnt = {
-        'address': 'cosmos1m9rhdryf059x684um3pa9n30tsdxuww84pxemz',
-        'coins': [{'denom': 'tru', 'amount': '0'}],
-        'sequence_number': '0',
-        'account_number':'0',
-        'original_vesting': [],
-        'delegated_free': [],
-        'delegated_vesting': [],
-        'start_time': '0',
-        'end_time': '0',
-        'module_name': 'stakeholder_tokens_pool',
-        'module_permissions': [],
-    }
-    genesis['app_state']['accounts'].append(stakeholderPoolAccnt)
+    # module accounts are automatically created...
 
     return genesis
 
