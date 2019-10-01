ba42a11f69e840c484102e462883efd761cd75ab
Shane Vitarana <shanev@gmail.com>
2019-09-30 22:06:10 -0700

    Fixed chain-id in migration

--- contrib/migration/16-token-inflation.py
+++ contrib/migration/16-token-inflation.py
@@ -88,7 +88,7 @@ def process_genesis(genesis, parsed_args):
 if __name__ == '__main__':
     parser = lib.init_default_argument_parser(
         prog_desc='Migrate genesis.json to adjust community tax',
-        default_chain_id='devnet-n',
+        default_chain_id='betanet-1',
         default_start_time='2019-02-11T12:00:00Z',
     )
     lib.main(parser, process_genesis)
